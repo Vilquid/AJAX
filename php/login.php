a6cd9fe4e044a69ec33a6080a996906496fdc4f2
sulfin <40837857+sulfin@users.noreply.github.com>
2020-12-01 11:09:30 +0100

    fix include for bootstrap

---
+++ php/login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/AJAX/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <title>Connexion / Inscription</title>
+</head>
+
+<body>
+    
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <a class=" navbar-brand" href="index.html">OuahJax</a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample03"
+            aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExample03">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+
+                    <a class="nav-link" href="/AJAX/index.php"><span><i class="fas fa-home"> </i></span class="sr-only">
+                        Accueil</a>
+                </li>
+
+
+                <li class="nav-item">
+                    <a class="nav-link" href="/AJAX/php/sujets.php"><span><i class="fab fa-wpforms"></i></span>
+                        Forum</a>
+                </li>
+            </ul>
+
+        </div>
+    </nav>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="/AJAX/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/bb5c883aee.js" crossorigin="anonymous"></script>
+    
+</body>
+
+</html>
