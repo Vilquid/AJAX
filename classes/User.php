59abde1cd17399c13492d66ea9790e3b39522809
Vilquid <60876471+Vilquid@users.noreply.github.com>
2020-12-05 18:24:02 +0100

    Update User
    Ajout des getters, setters et de hydrate
    45 min

--- classes/User.php
+++ classes/User.php
@@ -1,17 +1,56 @@
 <?php
 	class User
 	{
-		$email = " ";
-		// $password = "";
-		$pseudo = " ";
-		$id = 0;
+		private $_email = " ";
+		private $_pseudo = " ";
+		private $_id = 0;
 
-		public function __construct($data)
+		public function hydrate(array $data)
 		{
-			$this->pseudo = $data["pseudo"];
-			$this->email = $data["email"];
-			$this->id = $data["id"];
-			// $this->password = $data["password"];
+			foreach ($data as $key => $value) {
+				// On récupère le nom du setter correspondant à l'attribut.
+				$method = 'set_' . $key;
+	
+				// Si le setter correspondant existe.
+				if (method_exists($this, $method)) {
+					$this->$method($value);
+				}
+			}
+		}
+
+		public function __construct(array $data)
+		{
+			$this->hydrate($data);
+		}
+
+		public function set_pseudo($data)
+		{
+			$this->_pseudo = $data["pseudo"];
+		}
+
+		public function set_email($data)
+		{
+			$this->_email = $data["email"];
+		}
+
+		public function set_id($data)
+		{
+			$this->_id = $data["id"];
+		}
+
+		public function get_pseudo()
+		{
+			return $this->_pseudo;
+		}
+
+		public function get_email()
+		{
+			return $this->_email;
+		}
+
+		public function get_id()
+		{
+			return $this->_id;
 		}
 	}
 ?>
