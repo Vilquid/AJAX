b473c3d94acf8b0ce2c9b810e41696e885ad471f
sulfin <40837857+sulfin@users.noreply.github.com>
2020-12-30 04:13:13 +0100

    ajout des restriction côté serveur | 5min

--- ajax/addUser.php
+++ ajax/addUser.php
@@ -2,8 +2,12 @@
 require $_SERVER['DOCUMENT_ROOT'] . "/AJAX/classes/UserManager.php";
 if(isset($_POST['pseudo']) && isset($_POST['email']) && isset($_POST['password'])){
     $manager = new UserManager();
-    if($manager->addUser($_POST['pseudo'], $_POST['email'], $_POST['password'])){
-        echo 'success';
+    if(preg_match("/[0-9a-zA-Z_]{4,20}/",$_POST['pseudo']) && !preg_match("/(?:_.*){3,}/",$_POST['pseudo']) && preg_match("/.{6,}/",$_POST['password']) && preg_match("/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/",$_POST["email"])){
+        if($manager->addUser($_POST['pseudo'], $_POST['email'], $_POST['password'])){
+            echo 'success';
+        }else{
+            echo 'failed';
+        }
     }else{
         echo 'failed';
     }
