799b26c8ccd0013968e398e7baaef120bce304dd
sulfin <40837857+sulfin@users.noreply.github.com>
2021-01-01 20:05:09 +0100

    require en debut de page

--- index.php
+++ index.php
@@ -1,25 +1,5 @@
 <?php
-session_start();
-function chargerClasse($classe)
-{
-    require $_SERVER['DOCUMENT_ROOT'] . "/AJAX/classes/$classe.php";
-}
-spl_autoload_register('chargerClasse');
-$Usermanager = new UserManager();
-$ForumManager = new ForumManager();
-if (isset($_SESSION['user_id'])) {
-    if ($_SESSION['user_id'] != 0) {
-        $connected_user = $Usermanager->getUserClientById($_SESSION['user_id']);
-    } else {
-        $connected_user = null;
-    }
-} else {
-    $connected_user = null;
-}
-
-function connected(){
-    return (!$GLOBALS['connected_user'])?true:false;
-}
+require $_SERVER['DOCUMENT_ROOT'] . "/AJAX/require/pageInitialisation.php";
 
 ?>
 
