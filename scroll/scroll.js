f998dda34feb83be53c196f6a06e9ed791dc2cfe
Vilquid <60876471+Vilquid@users.noreply.github.com>
2021-01-03 23:15:36 +0700

    Code source scroll to the top

---
+++ scroll/scroll.js
@@ -0,0 +1,17 @@
+/* Scroll to top au clic sur la flèche */
+$(window).scroll(function() {
+	var height = $(window).scrollTop();
+	if (height > 100) {
+		$('#back2Top').fadeIn();
+	} else {
+		$('#back2Top').fadeOut();
+	}
+});
+
+$(document).ready(function() {
+	$("#back2Top").click(function(event) {
+		event.preventDefault();
+		$("html, body").animate({ scrollTop: 0 }, "slow");
+		return false;
+	});
+});
