96091794a2103a87d46352a69c9ae4ea9078cb47
sulfin <40837857+sulfin@users.noreply.github.com>
2021-01-03 21:52:28 +0100

    fix dans la mise en forme de l'heure

--- require/dateFormater.php
+++ require/dateFormater.php
@@ -2,19 +2,19 @@
 
 function getDureeAvecDateTime($datetime){
     $date = new DateTime($datetime);
-    $now = new DateTime('now');
+    $now = new DateTime('next hour');
     $diff = $now->diff($date);
     if($diff->y){
-        return $diff->format('%y an').($diff->y > 1)?'s':'';
+        return $diff->format('%y an').(($diff->y > 1)?'s':'');
     }else if($diff->m){
         return $diff->format('%m mois');
     }else if($diff->d){
         return $diff->format('%d jour').(($diff->d > 1)?'s':'');
     }else if($diff->h){
-        return $diff->format('%h heure').($diff->h > 1)?'s':'';
+        return $diff->format('%h heure').(($diff->h > 1)?'s':'');
     }else if($diff->i){
-        return $diff->format('%i minute').($diff->i > 1)?'s':'';
+        return $diff->format('%i minute').(($diff->i > 1)?'s':'');
     }else{
-        return $diff->format('%s seconde').($diff->s > 1)?'s':'';
+        return $diff->format('%s seconde').(($diff->s > 1)?'s':'');
     }
 }
