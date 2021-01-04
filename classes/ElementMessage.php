7938d729aaf7c5504479895475aa3083f2fbd62a
sulfin <40837857+sulfin@users.noreply.github.com>
2021-01-04 23:41:56 +0100

    Create ElementMessage.php

---
+++ classes/ElementMessage.php
@@ -0,0 +1,37 @@
+<?php
+class ElementSujet{
+    /**
+     * Fait pour la liste des messages sur la page post
+     */
+    
+     private $_id = 0;
+     private $_id_sujet = 0;
+     private $_message = '';
+     private $_id_user = 0;
+     private $_date = '';
+
+    
+    public function getId(){return $this->_id;}
+    public function setId($id){$this->_id=$id;}
+    public function getMessage(){
+        return htmlspecialchars($this->_message);
+    }
+    public function setMessage($message){$this->_message=$message;}
+    public function getId_user(){return $this->_id_user;}
+    public function setId_user($id_user){$this->_id_user=$id_user;}
+    public function getDate(){return $this->_date;}
+    public function setDate($date){$this->_date=$date;}
+    public function getId_sujet(){return $this->_id_sujet;}
+    public function setId_sujet($id_sujet){$this->_id_sujet=$id_sujet;}
+
+     private function hydrate($data) {
+        foreach ($data as $key => $value) {
+            $method = 'set'.ucfirst($key);
+            $this->$method($value);
+        }
+    }
+
+    public function __construct($data){
+        $this->hydrate($data);
+    }
+}
