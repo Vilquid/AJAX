4b2d632c4ea5dfc9977438fc801cf9b661c3743c
sulfin <40837857+sulfin@users.noreply.github.com>
2021-01-05 16:18:38 +0100

    supression tp parrain

--- TP Ajax du parrain/handlingData.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(isset($_POST["pseudo"]) && isset($_POST["pass"])){
-    if($_POST["pseudo"]=="pseudo" && $_POST["pass"]=="pass"){ // verif bdd
-        echo "OK";
-    }else{
-        echo "KO";
-    }
-}
